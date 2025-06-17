00df49e18822c95f67914fb41d11b846a7028d36
eddie.ho <eddie.ho@n-able.com>
2025-06-17 10:19:59 +0100

    .

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 terraform {
   required_version = "=1.5.5"
-  required_providers {
-    # aws = {
-    #   source = "hashicorp/aws"
-    #   version = "6.0.0-beta3"
-    # }
-  }
+#   required_providers {
+#     # aws = {
+#     #   source = "hashicorp/aws"
+#     #   version = "6.0.0-beta3"
+#     # }
+#   }
 }
 
 # provider "aws" {
