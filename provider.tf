dca5772516926677227f20d63cd65c8bcb7708f5
eddie.ho <eddie.ho@n-able.com>
2025-06-17 10:18:54 +0100

    remove provider

--- provider.tf
+++ provider.tf
@@ -1,13 +1,13 @@
 terraform {
   required_version = "=1.5.5"
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "6.0.0-beta3"
-    }
+    # aws = {
+    #   source = "hashicorp/aws"
+    #   version = "6.0.0-beta3"
+    # }
   }
 }
 
-provider "aws" {
-  # Configuration options
-}
+# provider "aws" {
+#   # Configuration options
+# }
